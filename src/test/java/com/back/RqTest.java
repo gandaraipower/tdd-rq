3e2177ee35d2dd860400b091603a368d45325cbc
gandaraipower <jjangjongsoo@naver.com>
2025-08-08 15:01:19 +0900

    ì이게 찐 첫번째 failed test

--- src/test/java/com/back/RqTest.java
+++ src/test/java/com/back/RqTest.java
@@ -1,4 +1,21 @@
 package com.back;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RqTest {
+
+
+    @Test
+    @DisplayName("getActionName()")
+    void t1(){
+
+        Rq rq = new Rq("삭제?id=1");
+
+        String actionName= rq.getActionName();
+
+        assertThat(actionName).isEqualTo("삭제");
+    }
 }
