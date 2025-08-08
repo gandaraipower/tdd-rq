f2c17df08e9053ca9546cba2aa3e73c2ac76fb2b
gandaraipower <jjangjongsoo@naver.com>
2025-08-08 15:18:43 +0900

    step2 blue

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -3,16 +3,18 @@ package com.back;
 public class Rq {
 
 
-//    void test(){ // default 접근 제어 아무것도 안붙인거,같은 패키지 안에서는 접근가능
-//
-//    }
+    private String cmd;
 
     public Rq(String cmd){
-
+        this.cmd = cmd;
     }
 
     public String getActionName(){
 
-        return "삭제";
+        //모르겠으면 의사코드를 이용해서 풀어보기
+        //? 기준 왼쪽을 잘라서 반환하면 된다.
+        //문자를 자르는 방법? -> gpt한테 물어보자
+       return cmd.split("\\?")[0];
+
     }
 }
